dfe46ebb5039fccd2d14ab059538a2a1d37514a4
Cristiano Aroxa <112488905+CristianoAroxa@users.noreply.github.com>
2024-05-21 21:52:01 -0300

    created cadCursos.html and new branch features

--- models/database.py
+++ models/database.py
@@ -20,6 +20,34 @@ class Aluna():
             'confirmaSenha': self.confirmaSenha
         })
 
+    @staticmethod
+    def get_all():
+        return list(mongo.db.alunas.find())
+    
+    @staticmethod
+    def get_by_id(id):
+        return mongo.db.alunas.find_one({'_id': ObjectId(id)})
+    
+    @staticmethod
+    def get_by_email(email):
+        return mongo.db.alunas.find_one({'email': email})
+    
+    @staticmethod
+    def delete(id):
+        mongo.db.alunas.delete_one({'_id': ObjectId(id)})
+
+    def update(self, id):
+        mongo.db.alunas.update_one(
+            {'_id': ObjectId(id)},
+            {'$set': {
+                'nome': self.nome,
+                'telefone': self.telefone,
+                'email': self.email,
+               'senha': self.senha,
+                'confirmaSenha': self.confirmaSenha
+            }}
+        )
+
 class Curso():
     def __init__(self, nome, descricao, valor, cargaHoraria):
         self.nome = nome
@@ -59,33 +87,7 @@ class Curso():
             }}
         )   
 
-    @staticmethod
-    def get_all():
-        return list(mongo.db.alunas.find())
     
-    @staticmethod
-    def get_by_id(id):
-        return mongo.db.alunas.find_one({'_id': ObjectId(id)})
-    
-    @staticmethod
-    def get_by_email(email):
-        return mongo.db.alunas.find_one({'email': email})
-    
-    @staticmethod
-    def delete(id):
-        mongo.db.alunas.delete_one({'_id': ObjectId(id)})
-
-    def update(self, id):
-        mongo.db.alunas.update_one(
-            {'_id': ObjectId(id)},
-            {'$set': {
-                'nome': self.nome,
-                'telefone': self.telefone,
-                'email': self.email,
-               'senha': self.senha,
-                'confirmaSenha': self.confirmaSenha
-            }}
-        )
         
 
         
